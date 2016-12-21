f2823a90819328222dc34ecbf2223aec4bfe6671
Felix <mimiyoyo1001@gmail.com>
2016-12-21 21:59:21 +0800

    Delete some method.

--- src/Model/UserData.java
+++ src/Model/UserData.java
@@ -44,13 +44,9 @@ public class UserData
         onlineUsers.add(count,new onlineUser(nickName,IP));
         count = count + 1;
     }
-    public static void delUser(String nickName)
+    /*public static void delUser(String nickName)
     {
-        if(onlineUsers.iterator().equals(nickName))
-        {
-            onlineUsers.remove(onlineUsers.listIterator().previousIndex()+1);
-        }
-    }
+    }*/
 
 }
 
